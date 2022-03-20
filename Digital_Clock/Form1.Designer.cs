ea7576ae0b1775da52581645c8a0b83ac4f3016e
mahmood seoud <Mahmoodseoud@gmail.com>
2022-03-20 13:41:38 +0100

    Initial push + finised app

---
+++ Digital_Clock/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿using System;
+
+namespace Digital_Clock
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.clockLabel = new System.Windows.Forms.Label();
+            this.dateLabel = new System.Windows.Forms.Label();
+            this.weekDayLabel = new System.Windows.Forms.Label();
+            this.secondsLabel = new System.Windows.Forms.Label();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // clockLabel
+            // 
+            this.clockLabel.AutoSize = true;
+            this.clockLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.clockLabel.Font = new System.Drawing.Font("DS-Digital", 72F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.clockLabel.ForeColor = System.Drawing.Color.White;
+            this.clockLabel.Location = new System.Drawing.Point(21, 24);
+            this.clockLabel.Name = "clockLabel";
+            this.clockLabel.Size = new System.Drawing.Size(251, 97);
+            this.clockLabel.TabIndex = 0;
+            this.clockLabel.Text = "22:22";
+            // 
+            // dateLabel
+            // 
+            this.dateLabel.AutoSize = true;
+            this.dateLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.dateLabel.Font = new System.Drawing.Font("DS-Digital", 36F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateLabel.ForeColor = System.Drawing.Color.White;
+            this.dateLabel.Location = new System.Drawing.Point(21, 134);
+            this.dateLabel.Name = "dateLabel";
+            this.dateLabel.Size = new System.Drawing.Size(308, 49);
+            this.dateLabel.TabIndex = 1;
+            this.dateLabel.Text = "MARCH 20 2022";
+            // 
+            // weekDayLabel
+            // 
+            this.weekDayLabel.AutoSize = true;
+            this.weekDayLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.weekDayLabel.Font = new System.Drawing.Font("DS-Digital", 36F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.weekDayLabel.ForeColor = System.Drawing.Color.White;
+            this.weekDayLabel.Location = new System.Drawing.Point(335, 134);
+            this.weekDayLabel.Name = "weekDayLabel";
+            this.weekDayLabel.Size = new System.Drawing.Size(190, 49);
+            this.weekDayLabel.TabIndex = 2;
+            this.weekDayLabel.Text = "weekDay";
+            // 
+            // secondsLabel
+            // 
+            this.secondsLabel.AutoSize = true;
+            this.secondsLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.secondsLabel.Font = new System.Drawing.Font("DS-Digital", 36F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.secondsLabel.ForeColor = System.Drawing.Color.White;
+            this.secondsLabel.Location = new System.Drawing.Point(278, 72);
+            this.secondsLabel.Name = "secondsLabel";
+            this.secondsLabel.Size = new System.Drawing.Size(70, 49);
+            this.secondsLabel.TabIndex = 3;
+            this.secondsLabel.Text = "22";
+            // 
+            // timer
+            // 
+            this.timer.Interval = 1000;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // DigitalClock
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(544, 202);
+            this.Controls.Add(this.secondsLabel);
+            this.Controls.Add(this.weekDayLabel);
+            this.Controls.Add(this.dateLabel);
+            this.Controls.Add(this.clockLabel);
+            this.Name = "DigitalClock";
+            this.Text = "Digital Clock";
+            this.Load += new System.EventHandler(this.DigitalClock_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+
+        #endregion
+
+        private System.Windows.Forms.Label clockLabel;
+        private System.Windows.Forms.Label dateLabel;
+        private System.Windows.Forms.Label weekDayLabel;
+        private System.Windows.Forms.Label secondsLabel;
+        private System.Windows.Forms.Timer timer;
+    }
+}
+
---
+++ Digital_Clock/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Digital_Clock
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            clockLabel.Text = DateTime.Now.ToString("HH:MM");
+            secondsLabel.Text = DateTime.Now.ToString("ss");
+            dateLabel.Text = DateTime.Now.ToString("dd MMMM yyyy");
+            weekDayLabel.Text = DateTime.Now.ToString("dddd");
+            secondsLabel.Location = new Point(clockLabel.Location.X + clockLabel.Width, clockLabel.Location.Y);
+
+        }
+
+        private void DigitalClock_Load(object sender, EventArgs e)
+        {
+            timer.Start();
+        }
+    }
+}
